e5560f4718a40f0508fb33cf4b51dc464e8e4b82
cheng <1240800450@qq.com>
2023-05-27 22:37:30 +0800

    栈

---
+++ 栈/数组实现栈.go
@@ -0,0 +1,84 @@
+// 支持随机访问，查询速度快，但存在元素在数组空间中大量移动的操作，增删效率低。
+// 这个增删操作的不就是尾部嘛，用切片感觉效率很高呀
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type ArrayStack struct {
+	array []string
+	size  int
+	lock  sync.Mutex
+}
+
+// 入栈
+func (stack *ArrayStack) Push(v string) {
+	stack.lock.Lock()
+	defer stack.lock.Unlock()
+
+	// 放入切片中，后进的元素放在数组最后面
+	stack.array = append(stack.array, v)
+	stack.size = stack.size + 1
+}
+
+// 出栈
+func (stack *ArrayStack) Pop() string {
+	stack.lock.Lock()
+	stack.lock.Unlock()
+
+	if stack.size == 0 {
+		panic("empty")
+	}
+
+	// 栈顶元素
+	v := stack.array[stack.size-1]
+
+	// 方式1：切片收缩，但是切片占用的容量不变，所以占用的内存不会减少
+	stack.array = stack.array[0 : stack.size-1]
+
+	// 方式2：创建新的数组，新数组的容量就是元素个数,但是时间复杂度增加
+	// newArray := make([]string, stack.size-1, stack.size-1)
+	// for i := 0; i < stack.size-1; i++ {
+	// 	newArray[i] = stack.array[i]
+	// }
+	// stack.array = newArray
+
+	stack.size = stack.size - 1
+	return v
+}
+
+// 获取栈顶元素
+func (stack *ArrayStack) Peek() string {
+	// 栈中元素已空
+	if stack.size == 0 {
+		panic("empty")
+	}
+
+	v := stack.array[stack.size-1]
+	return v
+}
+
+// 栈大小
+func (stack *ArrayStack) Size() int {
+	return stack.size
+}
+
+// 栈是否为空
+func (stack *ArrayStack) IsEmpty() bool {
+	return stack.size == 0
+}
+
+func main() {
+	arrayStack := new(ArrayStack)
+	arrayStack.Push("cat")
+	arrayStack.Push("dog")
+	arrayStack.Push("hen")
+	fmt.Println("size:", arrayStack.Size())
+	fmt.Println("pop:", arrayStack.Pop())
+	fmt.Println("pop:", arrayStack.Pop())
+	fmt.Println("size:", arrayStack.Size())
+	arrayStack.Push("drag")
+	fmt.Println("pop:", arrayStack.Pop())
+}
---
+++ 栈/链表实现栈.go
@@ -0,0 +1,91 @@
+// 只支持顺序访问，在某些遍历操作中查询速度慢，但增删元素快
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// 链表节点
+type LinkNode struct {
+	Next  *LinkNode
+	Value string
+}
+
+// 链表栈
+type LinkStack struct {
+	root *LinkNode // 链表起点
+	size int
+	lock sync.Mutex
+}
+
+// 入栈
+func (stack *LinkStack) Push(v string) {
+	stack.lock.Lock()
+	defer stack.lock.Unlock()
+
+	// 如果栈顶为空，那么增加节点
+	if stack.root == nil {
+		stack.root = new(LinkNode)
+		stack.root.Value = v
+	} else {
+		preNode := stack.root
+
+		newNode := new(LinkNode)
+		newNode.Value = v
+
+		// 头插法
+		newNode.Next = preNode
+		// 更新头部
+		stack.root = newNode
+	}
+	stack.size = stack.size + 1
+}
+
+// 出栈
+func (stack *LinkStack) Pop() string {
+	stack.lock.Lock()
+	defer stack.lock.Unlock()
+
+	if stack.size == 0 {
+		panic("stack is empty")
+	}
+
+	topNode := stack.root
+	v := topNode.Value
+	// 更新头部
+	stack.root = topNode.Next
+	stack.size = stack.size - 1
+	return v
+}
+
+// 获取栈顶元素
+func (stack *LinkStack) Peek() string {
+	if stack.size == 0 {
+		panic("empty")
+	}
+	return stack.root.Value
+}
+
+// 栈大小
+func (stack *LinkStack) Size() int {
+	return stack.size
+}
+
+// 栈是否为空
+func (stack *LinkStack) IsEmpty() bool {
+	return stack.size == 0
+}
+
+func main() {
+	linkStack := new(LinkStack)
+	linkStack.Push("cat")
+	linkStack.Push("dog")
+	linkStack.Push("hen")
+	fmt.Println("size:", linkStack.Size())
+	fmt.Println("pop:", linkStack.Pop())
+	fmt.Println("pop:", linkStack.Pop())
+	fmt.Println("size:", linkStack.Size())
+	linkStack.Push("drag")
+	fmt.Println("pop:", linkStack.Pop())
+}
