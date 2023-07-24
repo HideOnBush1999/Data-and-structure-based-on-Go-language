f9b148fa56419743d448339e18216dc77dc01f3f
cheng <1240800450@qq.com>
2023-07-24 20:01:55 +0800

    AVL树

--- j.查找算法/哈希函数.go
+++ j.查找算法/哈希函数.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/OneOfOne/xxhash"
 )
 
@@ -11,11 +13,10 @@ func XXHash(key []byte) uint64 {
 	return h.Sum64()
 }
 
-/* func main() {
+func main() {
 	keys := []string{"hi", "my", "friend", "I", "love", "you", "my", "apple"}
 	for _, key := range keys {
 		// []byte(key) 是将字符串 key 转换为字节切片的操作
-		fmt.Printf("xxhash('%s')=%d\n", key, XXHash([]byte(key)))    // 对于对应的字符串，每次计算出来的值是固定不变的
+		fmt.Printf("xxhash('%s')=%d\n", key, XXHash([]byte(key))) // 对于对应的字符串，每次计算出来的值是固定不变的
 	}
 }
-*/
